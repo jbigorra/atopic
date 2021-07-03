a662ceb52cbf092bd8c2cad0757d0297b73b5001
Juan bigorra <juanchoibf@gmail.com>
2021-07-03 22:53:56 +0200

    Create config file to gather all env variables in one place

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export const config = {
+  SUPABASE_PUBLIC_KEY: import.meta.env.SUPABASE_PUBLIC_KEY,
+  SUPABASE_HOST: import.meta.env.SUPABASE_HOST,
+  SUPABASE_PRIVATE_KEY: import.meta.env.SUPABASE_PRIVATE_KEY,
+  DATABASE_CONN: import.meta.env.DATABASE_CONN,
+  EMAIL_HOST: import.meta.env.EMAIL_HOST,
+}
