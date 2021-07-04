e5d3c941ac07047938570eea0cf7bf15250ad46c
Juan bigorra <juanchoibf@gmail.com>
2021-07-04 02:37:37 +0200

    Add basic hardcoded cards, navbar and searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
+import { Navbar } from "@components/navbar/Navbar";
 import * as React from "react";
+import './styles.scss';
+
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Navbar />
+      <div className="container is-fluid">
+        <div className="columns">
+          <div className="column">
+            <div className="field">
+              <p className="control has-icons-left">
+                <input className="input is-big is-primary" type="text" placeholder="Search..." />
+                <span className="icon is-small is-left is-flex is-align-items-center is-justify-content-center">
+                  <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-reactroot="">
+                    <path fill="#85D0CC" d="M13.0002 12.9999C13.3907 12.6094 14.0239 12.6094 14.4144 12.9999L20.7072 19.2928C21.0977 19.6833 21.0977 20.3165 20.7072 20.707C20.3166 21.0975 19.6835 21.0975 19.2929 20.707L13.0002 14.4141C12.6096 14.0236 12.6096 13.3904 13.0002 12.9999Z" clipRule="evenodd" fillRule="evenodd" strokeWidth="1.5"></path>
+                    <path fill="#98EED5" d="M14.2426 5.75736C16.5858 8.1005 16.5858 11.8995 14.2426 14.2426C11.8995 16.5858 8.1005 16.5858 5.75736 14.2426C3.41421 11.8995 3.41421 8.1005 5.75736 5.75736C8.1005 3.41421 11.8995 3.41421 14.2426 5.75736Z" strokeWidth="1.5"></path>
+                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="#fcffff" d="M6.46439 10.707C6.46439 10.707 6.46439 11.4141 7.52505 12.4748C8.58571 13.5354 9.29282 13.5354 9.29282 13.5354"></path>
+                  </svg>
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="column is-2 is-mobile">
+            <div className="card">
+              <div className="card-header">
+                <p className="card-header-title">Software Craftmanship</p>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  Saved items: 5
+                  <br></br>
+                  Tags: #SOFTWARE #DDD #TDD
+                </div>
+              </div>
+              <div className="card-footer">
+                <button className="button is-link is-fullwidth is-square">Edit</button>
+              </div>
+            </div>
+          </div>
+          <div className="column is-2 is-mobile">
+            <div className="card">
+              <div className="card-header">
+                <p className="card-header-title">Software Craftmanship</p>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  Saved items: 5
+                  <br></br>
+                  Tags: #SOFTWARE #DDD #TDD
+                </div>
+              </div>
+              <div className="card-footer">
+                <button className="button is-link is-fullwidth is-square">Edit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+export function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function showNavbarMenu() {
+    setIsVisible(s => !s);
+  }
+
+  const activeClass = isVisible ? "is-active" : "";
+
+  return (
+    <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="#">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        </a>
+
+        <a role="button" onClick={showNavbarMenu} className={`navbar-burger ${activeClass}`} aria-label="menu" aria-expanded="false" data-target="navbar-content">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbar-content" className={`navbar-menu ${activeClass}`}>
+        <div className="navbar-start">
+          <a className="navbar-item">
+            Home
+          </a>
+
+          <a className="navbar-item">
+            Documentation
+          </a>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
