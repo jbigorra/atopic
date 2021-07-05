f97f72a432ad238faf63b8ed16c8b98ad9039795
Juan bigorra <juanchoibf@gmail.com>
2021-07-05 15:39:21 +0200

    Extract search bar and icon and create own components

---
+++ src/components/forms/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { MagnifyingGlassIcon } from '@components/icons'
+import * as React from 'react'
+
+export function SearchBar() {
+  return (
+    <div className="field has-addons">
+      <p className="control has-icons-left is-expanded">
+        <input className="input is-big is-primary" type="text" placeholder="Search..." />
+        <span className="icon is-small is-left is-flex is-align-items-center is-justify-content-center">
+          <MagnifyingGlassIcon />
+        </span>
+      </p>
+      <div className="control">
+        <button type="submit" className="button is-primary">Search</button>
+      </div>
+    </div>
+  )
+}
