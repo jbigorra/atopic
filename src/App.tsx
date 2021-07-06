395ce2f6e605b4fc21089712e6e0f939bb7078b2
Juan bigorra <juanchoibf@gmail.com>
2021-07-06 02:01:15 +0200

    Add basic routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { Navbar } from "@components/navbar/Navbar";
 import { DashboardView } from "@views/dashboard";
 import * as React from "react";
+import { Link, Redirect, Route, Switch } from "react-router-dom";
 import './styles.sass';
 
 function App() {
   return (
     <>
       <Navbar />
-      <DashboardView />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/dashboard"/>
+        </Route>
+        <Route path="/auth">
+          <div className="content container is-fluid">
+            <p>Auth flow not implemented</p>
+            <Link to="/">Return</Link>
+          </div>
+        </Route>
+        <Route path="/dashboard">
+          <DashboardView />
+        </Route>
+      </Switch>
     </>
   );
 }
