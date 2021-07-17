51dcd4eb60e519a9e257a95cdc405dac3827b07e
Juan bigorra <juanchoibf@gmail.com>
2021-07-17 20:03:46 +0200

    Add cancel and save buttons

--- src/views/dashboard/ResourceCreationView.tsx
+++ src/views/dashboard/ResourceCreationView.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import CategorySelectInput from './components/CategorySelectInput';
 
@@ -8,16 +9,22 @@ const fakeOptions = [
   { id: Date.now().toString(), category: "Clean code" },
 ];
 
+function handleSubmit(e: FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  const form = new FormData(e.currentTarget);
+  console.log(form);
+}
+
 export default function ResourceCreationView() {
   return (
     <div className="container is-fluid">
       <div className="columns">
         <div className="is-fullwidth">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="field">
               <label className="label" htmlFor="resource-url">Resource URL</label>
               <div className="control">
-                <input id="resource-url" name="resource-url" className="input" type="url" placeholder="e.g. https://www.youtube.com/watch?v=someVideoId..." />
+                <input id="resource-url" name="resource-url" className="input" type="url" placeholder="e.g. https://www.youtube.com/watch?v=someVideoId..." required/>
               </div>
             </div>
             <CategorySelectInput initialOptions={fakeOptions}/>
@@ -27,8 +34,15 @@ export default function ResourceCreationView() {
                 <input id="tags" name="tags" className="input" type="text" placeholder="e.g. Software Engineering" />
               </div>
             </div>
+            <div className="field is-grouped">
+              <p className="control">
+                <Link className="button is-danger is-outlined" to="/dashboard">Cancel</Link>
+              </p>
+              <p>
+                <button type="submit" className="button is-primary">Save</button>
+              </p>
+            </div>
           </form>
-          <Link to="/dashboard">Return</Link>
         </div>
       </div>
     </div>
